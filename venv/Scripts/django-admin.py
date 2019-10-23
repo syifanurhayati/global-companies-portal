5da40f276b8b0097e34041d7b6ab1cfad3c4a83b
JessiJ <jessicana69@gmail.com>
2019-10-24 01:13:04 +0700

    Initial Project Structure

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\kuliah\s2\semweb\tugas kelompok\global-companies-portal\venv\scripts\python3.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
