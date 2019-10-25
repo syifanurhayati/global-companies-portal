dc19896dbff6cb7c69e9ab34123ee727d6a8c41b
JessiJ <jessicana69@gmail.com>
2019-10-26 00:03:36 +0700

    change Django setting

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+	'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': '',
+		'HOST': 'localhost',
+		'PORT': '3306',
     }
 }
 
