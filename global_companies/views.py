931179340adb0e6fa7f7027fca13b36e9d2990ec
JessiJ <jessicana69@gmail.com>
2019-11-04 07:32:23 +0700

    modify homepage so that search button reffered to searchpage

--- global_companies/views.py
+++ global_companies/views.py
@@ -2,15 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 # def myView(request):
 # 	return HttpResponse("Hello, World!")
 
 def viewHomePage(request):
-	if(request.method == 'GET'):
-		search_query = request.GET.get('search_box', None)
 	return render(request, 'homepage.html')
 
 def viewSearchPage(request):
 	return render(request, 'searchpage.html')
+
+def search(request):
+	search_query = ""
+	if(request.method == 'GET'):
+		search_query = request.GET.get('search_box', None)
+	return HttpResponse(search_query)
